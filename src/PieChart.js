5b2126abd341f41064ba38578f41bbec68925582
HFZ <hfz@autoreg-197063.dyn.wpi.edu>
2019-04-15 18:21:32 -0400

    move data parser to constructor

--- src/PieChart.js
+++ src/PieChart.js
@@ -15,7 +15,9 @@ const months = ["January", "February", "March", "April", "May", "June", "July", 
 const COLORS = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042'];
 
 export default class App extends Component{
-    render () {
+    constructor(props) {
+        super(props);
+
         let thisMonthSpending = this.props.data;
 
         let categoryCount = {};
@@ -36,34 +38,40 @@ export default class App extends Component{
             categoryCountList.push(item)
         });
 
+        this.state = {data: categoryCountList}
+    }
+
+    renderCustomizedLabel = ({ cx, cy, midAngle, innerRadius, outerRadius, percent, index}) => {
         const RADIAN = Math.PI / 180;
-        const renderCustomizedLabel = ({ cx, cy, midAngle, innerRadius, outerRadius, percent, index }) => {
-            const radius = innerRadius + (outerRadius - innerRadius)*0.5;
-            const x  = cx + radius * Math.cos(-midAngle * RADIAN);
-            const y = cy  + radius * Math.sin(-midAngle * RADIAN);
 
-            const catRadius = innerRadius + (outerRadius - innerRadius)*1.5;
-            const catX  = cx + catRadius * Math.cos(-midAngle * RADIAN);
-            const catY = cy  + catRadius * Math.sin(-midAngle * RADIAN);
+        const radius = innerRadius + (outerRadius - innerRadius)*0.5;
+        const x  = cx + radius * Math.cos(-midAngle * RADIAN);
+        const y = cy  + radius * Math.sin(-midAngle * RADIAN);
 
-            return (
-                <text x={x} y={y} textAnchor={x > cx ? 'start' : 'end'} >
-                    <tspan x={x} y={y} fill="white">
-                        {(percent * 100).toFixed(0)}%
-                    </tspan>
-                    <tspan x={catX} y={catY} fill="black">
-                        {categoryCountList[index].category}
-                    </tspan>
-                </text>
-            );
-        };
-
+        const catRadius = innerRadius + (outerRadius - innerRadius)*1.5;
+        const catX  = cx + catRadius * Math.cos(-midAngle * RADIAN);
+        const catY = cy  + catRadius * Math.sin(-midAngle * RADIAN);
 
         return (
+            <text x={x} y={y} textAnchor={x > cx ? 'start' : 'end'} >
+                <tspan x={x} y={y} fill="white">
+                    {(percent * 100).toFixed(0)}%
+                </tspan>
+                <tspan x={catX} y={catY} fill="black">
+                    {this.state.data[index].category}
+                </tspan>
+            </text>
+        );
+    };
+
+
+
+    render () {
+        return (
                 <PieChart height={400} width={550} className="">
-                    <Pie animationDuration={700} data={categoryCountList} dataKey="count" nameKey="category" cx="50%" cy="50%" innerRadius={80} outerRadius={140} fill="#8884d8" label={renderCustomizedLabel} >
+                    <Pie animationDuration={700} data={this.state.data} dataKey="count" nameKey="category" cx="50%" cy="50%" innerRadius={80} outerRadius={140} fill="#8884d8" label={this.renderCustomizedLabel} >
                              {
-                                 categoryCountList.map((entry, index) => <Cell fill={COLORS[index % COLORS.length]}/>)
+                                 this.state.data.map((entry, index) => <Cell fill={COLORS[index % COLORS.length]}/>)
                              }
                     </Pie>
                 </PieChart>
